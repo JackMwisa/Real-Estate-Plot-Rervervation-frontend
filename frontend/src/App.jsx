e35b99470bee1b62f01c95f85bae72ebf044c58e
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2025-08-05 12:43:28 +0200

    Refactor App and add Navbar component
    Removed default Vite/React content and styles from App.jsx, App.css, and index.css. Added a new Navbar component and updated the app title in index.html to 'UG Real Estate'.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  function myFunction() {
+    alert('Hello World');
+  }
+
+  const myComponent = () => {
+    return <h1>This is a component</h1>;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>This is an h1</h1>
+      <p>this is is a paragraph</p>
+      {myComponent()}
+      <button onClick={myFunction}>Click me</button>
+
+      <myComponent />
+      <myComponent />
+
+      {myComponent()}
+
+      <Navbar />
+
     </>
   )
 }
